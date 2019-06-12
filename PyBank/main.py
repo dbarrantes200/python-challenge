795ab2561ec83f07957743f23756a31911b2981a
Daniel Barrantes <dbarrantes200@yahoo.com>
2019-06-12 18:27:01 -0400

    logic for showing dates for greatest decrease and greatest increase added

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,25 +6,43 @@ budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter=",")
     totals = []
-    
+        
     for row_count, row in enumerate(csvreader, start=1):
         budget = int(row['Profit/Losses'])
         totals.append(budget)
         avg = sum(totals) / row_count
-       
 
+
+# The greatest increase in profits (date and amount) over the entire period
+       
+Greatest = max(totals)
+Decrease = min(totals)
+
+with open(budget_csv, 'r') as csvfile:
+   csvreader = csv.DictReader(csvfile, delimiter=",")
+   for row_count, row in enumerate(csvreader, start=1):
+       if int(row['Profit/Losses']) == Greatest:
+            Greatest =  int(row['Profit/Losses'])
+            date = (row['Date'])
+# The greatest decrease in losses (date and amount) over the entire period
+       if int(row['Profit/Losses']) == Decrease:
+            Decrease =  int(row['Profit/Losses'])
+            date1 = (row['Date'])
+           
+# Screen Financial Analysis Ouput
 
 print ("Financial Analysis")
-print ("-------------------------------")
+print ("--------------------------------------")
 print ("Total Months: {}".format(row_count))
 print ("Total: ${}".format(sum(totals)))
 print ("Average Change: $", format(avg, ",.2f"))
-print ("Greatest Increase in Profits: ${}".format(max(totals)))
-print ("Greatest Decrease in Profits: ${}".format(min(totals)))
+print ("Greatest Increase in Profits: ", (date),"$", (Greatest))
+print ("Greatest Decrease in Profits: ", (date1),"$", (Decrease))
+
 
 output_file = os.path.join("Financial Analysis Report.txt")
 
-#  Open the output file
+#  Writing the output file
 with open(output_file, "w") as text_file:
     text_file.write ("Financial Analysis\n")
     text_file.write ("-------------------------------\n")
